236e63dfa1120f8f405f053cf8f2e9a1add9f866
Michael Moll <kvedulv@kvedulv.de>
2016-04-02 21:09:12 +0200

    use file_line for bootstrap.cfg

--- manifests/server/puppetserver.pp
+++ manifests/server/puppetserver.pp
@@ -69,7 +69,6 @@ class puppet::server::puppetserver (
   $server_ca_client_whitelist  = $::puppet::server_ca_client_whitelist,
   $server_admin_api_whitelist  = $::puppet::server_admin_api_whitelist,
 ) {
-  require ::puppet::server::augeaslens
   include ::puppet::server
 
   $puppetserver_package = pick($::puppet::server_package, 'puppetserver')
@@ -96,11 +95,26 @@ class puppet::server::puppetserver (
                 'set @simple[. = "puppetlabs.services.ca.certificate-authority-disabled-service"]/@value certificate-authority-disabled-service',],
   }
 
-  augeas { 'puppet::server::puppetserver::server_ca':
-    context => "/files${server_puppetserver_dir}/bootstrap.cfg",
-    changes => $augcmds,
-    incl    => "${server_puppetserver_dir}/bootstrap.cfg",
-    lens    => 'Trapperkeeper.lns',
+  $ca_enabled_ensure = $server_ca ? {
+    true    => present,
+    default => absent,
+  }
+
+  $ca_disabled_ensure = $server_ca ? {
+    false   => present,
+    default => absent,
+  }
+
+  file_line { 'ca_enabled':
+    ensure => $ca_enabled_ensure,
+    path   => "${server_puppetserver_dir}/bootstrap.cfg",
+    line   => 'puppetlabs.services.ca.certificate-authority-service/certificate-authority-service',
+  }
+
+  file_line { 'ca_disabled':
+    ensure => $ca_disabled_ensure,
+    path   => "${server_puppetserver_dir}/bootstrap.cfg",
+    line   => 'puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service',
   }
 
   file { "${server_puppetserver_dir}/conf.d/ca.conf":
